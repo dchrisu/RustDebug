fdb75dcce5a5351efb36caf10298f3efe0f23bc3
Christopher Dang <dchrisu@yahoo.com>
2019-03-10 18:02:57 -0500

    Update Debug3_solution.rs

--- Debug3_solution.rs
+++ Debug3_solution.rs
@@ -1,3 +1,11 @@
+/*!
+    This is the solution of our problem. We simply had
+    to replace our operator from a multiplication (*) sign
+    to an addition (+) sign. Now we add all of our 
+    modulus 3 and 5 elements from our vector together 
+    in our for loop.
+*/
+
 fn main() {
     println!("Printing multiples all numbers divisible between 1 and 1000 of 3 and 5");
     let mut numbers: Vec<u128> = Vec::new();
